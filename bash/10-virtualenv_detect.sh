2839b6c18279feaca11988cc59a622ea7962e106
Jesse Dhillon <jesse@dhillon.com>
2015-03-29 10:19:41 -0700

    Improved virtualenv detection

--- bash/10-virtualenv_detect.sh
+++ bash/10-virtualenv_detect.sh
@@ -1,9 +1,21 @@
 function venv_prompt()
 {
-  if [ "$PWD" != "$OLDPWD" ]; then
-    OLDPWD="$PWD"
-    test -e .virtualenv && workon `cat .virtualenv`
-  fi
+  DIR=$PWD
+  while true; do
+    if [ -e "${DIR}/.virtualenv" ]; then
+      workon `cat ${DIR}/.virtualenv`
+      break
+    fi
+
+    if [ "$DIR" == "/" ]; then
+      if [ -n "$VIRTUAL_ENV" ]; then
+        deactivate
+      fi
+      break
+    fi
+
+    DIR=$(dirname $DIR)
+  done
 }
 
 PROMPT_COMMANDS=(${PROMPT_COMMANDS[@]} 'venv_prompt')
