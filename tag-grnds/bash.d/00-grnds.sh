10fddf936dfb1ce50d093c3e140b726ec10cb157
Jesse Dhillon <jesse@dhillon.com>
2015-09-23 09:40:20 -0700

    Adds IPSEC_SECRETS_FILE and KEY_SUFFIX to grnds

--- tag-grnds/bash.d/00-grnds.sh
+++ tag-grnds/bash.d/00-grnds.sh
@@ -1,6 +1,8 @@
 # grnds
 export GR_USERNAME=jesse.dhillon
 export GR_HOME=~/Devel
+export IPSEC_SECRETS_FILE=/usr/local/etc/ipsec.secrets
+export KEY_SUFFIX=grandroundshealth.com
 
 if [ -d "${GR_HOME}/engineering" ]; then
     PATH="${GR_HOME}/engineering/bin:${PATH}"
