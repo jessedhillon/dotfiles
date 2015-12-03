e236ae29b5daec94b6c39f49e786d7e53e8808cc
Jesse Dhillon <jesse@dhillon.com>
2015-12-03 11:27:33 -0800

    Adds *.json ignore to ag alias

--- bash.d/00-aliases.sh
+++ bash.d/00-aliases.sh
@@ -2,7 +2,7 @@
 
 alias xclip="xclip -selection clipboard"
 alias rm="trash"
-alias ag="ag --ignore *.log --ignore *.map --ignore *.dump --ignore *.sql --pager='less -RX'"
+alias ag="ag --ignore *.log --ignore *.map --ignore *.dump --ignore *.sql --ignore *.json --pager='less -RX'"
 alias less="less -RX"
 alias bc="bc -i ~/.bcrc"
 alias ls="ls -h --color=auto"
