a3c6fe68d852e9328133546019245cbfb260d4c3
Jesse Dhillon <jesse@dhillon.com>
2016-01-04 09:42:29 -0800

    Update sophia credentials

--- tag-private/bash.d/00-sophia.sh
+++ tag-private/bash.d/00-sophia.sh
@@ -2,10 +2,9 @@ export SOPHIA_DATABASE_URL=$(cat << EOF | gpg --dearmor
 -----BEGIN PGP ARMORED FILE-----
 Version: GnuPG v1
 
-bXlzcWwyOi8vamVzc2UuZGhpbGxvbjo3R3F4OGl0TUdPaURmcFRAc29waGlhZGIu
-Y3RyZ2l0ZG1oM2puLnVzLWVhc3QtMS5yZHMuYW1hem9uYXdzLmNvbTozMzA2L3Nv
-cGhpYQo=
-=RiX/
+bXlzcWwyOi8vamVzc2UuZGhpbGxvbjo3R3F4OGl0TUdPaURmcFRAaHEtbXlzcWwu
+YW5hbHl0aWNzLmdyYW5kcm91bmRzLmNvbTozMzA2L3NvcGhpYQo=
+=b3LR
 -----END PGP ARMORED FILE-----
 EOF
 )
