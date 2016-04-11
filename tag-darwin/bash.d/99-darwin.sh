829e072775faa9715c1e2d14ec2ec23d88dc9b63
Jesse Dhillon <jesse@dhillon.com>
2016-04-11 13:30:34 -0700

    iQIcBAABAgAGBQJXDAmEAAoJEEgbiExRItKM0ZAP/i2jpQGa9Ve3E6nNYKCMs58f  8muLF0DqILsqoQITH42GzBG9zcmZLyMcZKwDUFXuKkbTWRkpqh+jlwkSXq4e+c+v  UtEFxVduU2HD3zNnfHG9qFPBM1pD6uxLONf2DMwsYCGc0ki6JNCtab/uQyzX8zVZ  vU5pg+1C17q5BLLd9HN+oZFp5fIIzd7HrdAgaSzrBdAdlFda8u3b6l+5Gg/P/EUN  0LswZcXL0V3jYHKZdhOCrt2o7Z2EBfE2FE+U6Q5U3fJkGCCfMcX8J8BfKwcsh0Ov  EiJYiCjsQcCliJ3EfE7BSaaHgLiMtxruvkM5TafxvUe9FMdRItenl4yezJWusJ6p  mYpilMRUJWzNZCu+LHbBNymoGboDJdDcKvaSVCgxAdywp3T1k0cC88LzoyzQ24ba  X+wixWhV5gIv1i9RLz/Ozxi0yRw5Y0QwFg1ESmirTWLeBDxJdzffkWhItaa+OCQo  NHX1glxhp2RTUdiOopdkxM7HDG+NSPDdQX1cZ1++3+Ln5zQdWoUxDJ9GkxiU4p8Z  rJByHwTa0+R532eZ/4tgoKL5xCctmsv2rdtXnOl87RA6fl3BesIyNNmrf+RY3Chs  AuFsGR9FCz9CvYEY18Ek40xFPt7PK58yFmnbBBUPQ0UPjcv+Q8G/yttORtjF3uQw  /jDeettGa0qUUtSmwZUk  =D2tx  -----END PGP SIGNATURE-----
    Adds bash completion sourcing logic to darwin

--- tag-darwin/bash.d/99-darwin.sh
+++ tag-darwin/bash.d/99-darwin.sh
@@ -1,7 +1,21 @@
 if [[ $OSTYPE =~ ^darwin ]]; then
+    if [ -f /usr/local/etc/bash_completion ]; then
+        . /usr/local/etc/bash_completion
+    fi
+
     if [ -f /usr/local/git/contrib/completion/git-completion.bash ]; then
         . /usr/local/git/contrib/completion/git-completion.bash
+    elif [ -f $(xcode-select -p)/usr/share/git-core/git-completion.bash ]; then
+        . $(xcode-select -p)/usr/share/git-core/git-completion.bash
+    fi
+
+    if [ -f $(xcode-select -p)/usr/share/git-core/git-prompt.sh ]; then
+        . $(xcode-select -p)/usr/share/git-core/git-prompt.sh
     fi
 
     alias ls="ls -h -G"
+    alias vim="mvim -v"
+    if [ -x /Applications/Xcode.app/Contents/Developer/usr/bin/git ]; then
+        alias git="/Applications/Xcode.app/Contents/Developer/usr/bin/git"
+    fi
 fi
