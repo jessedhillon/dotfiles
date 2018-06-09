5220bb386739845b01bc361bffe4b92c17e80a20
Jesse Dhillon <jesse@dhillon.com>
2018-06-09 09:38:59 -0700

    remove tag-grnds

--- tag-grnds/bash.d/00-tracker.sh
+++
@@ -1,9 +0,0 @@
-export TRACKER_TOKEN=$(cat << EOF | gpg --dearmor
------BEGIN PGP ARMORED FILE-----
-Version: GnuPG v1
-
-ZmNjNmFiMGEwOGNjMzQ2NjUwMGNjZGZkNDRhMTU4MTYK
-=YIF5
------END PGP ARMORED FILE-----
-EOF
-)
