f8e7d170a10d8c721926c01997b7e20a693906d7
Jesse Dhillon <jdhillon@plaid.com>
2018-10-30 16:51:05 -0700

    [plaid] update envvars

--- tag-plaid/bash.d/99-plaid.sh
+++ tag-plaid/bash.d/99-plaid.sh
@@ -5,3 +5,5 @@ export PLAID_SECRET="7b5edb6a5035a8d5c56bdb20632fc0"
 export PLAID_PUBLIC_KEY="e133b937c62ee47787762a8046bb08"
 export PLAID_ENV="development"
 
+export GITHUB_TOKEN="51f56a67af57d004aa138aeddf1230a760078d82"
+export GOPATH=$HOME/Devel/golang
