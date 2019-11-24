9634f4918f0b6f70c7f739a77d4b3970847f4df2
Jesse Dhillon <jdhillon@plaid.com>
2019-11-23 22:17:15 -0800

    [darwin] update for macports version bump

--- tag-darwin/bash.d/99-darwin.sh
+++ tag-darwin/bash.d/99-darwin.sh
@@ -1,5 +1,5 @@
 if [[ $OSTYPE =~ ^darwin ]]; then
-    export PATH=/opt/local/bin:$PATH
+    export PATH=/opt/local/bin:/opt/local/sbin:$PATH
 
     if [ -f /opt/local/etc/bash_completion ]; then
         . /opt/local/etc/bash_completion
@@ -16,9 +16,9 @@ if [[ $OSTYPE =~ ^darwin ]]; then
     alias ls="ls -h -G"
 fi
 
-if [ -f /opt/local/bin/virtualenvwrapper.sh-3.6 ]; then
-    export VIRTUALENVWRAPPER_PYTHON='/opt/local/bin/python3.6'
-    export VIRTUALENVWRAPPER_VIRTUALENV='/opt/local/bin/virtualenv-3.6'
-    export VIRTUALENVWRAPPER_VIRTUALENV_CLONE='/opt/local/bin/virtualenv-clone-3.6'
-    source /opt/local/bin/virtualenvwrapper.sh-3.6
+if [ -f /opt/local/bin/virtualenvwrapper.sh-3.7 ]; then
+    export VIRTUALENVWRAPPER_PYTHON='/opt/local/bin/python3.7'
+    export VIRTUALENVWRAPPER_VIRTUALENV='/opt/local/bin/virtualenv-3.7'
+    export VIRTUALENVWRAPPER_VIRTUALENV_CLONE='/opt/local/bin/virtualenv-clone-3.7'
+    source /opt/local/bin/virtualenvwrapper.sh-3.7
 fi
