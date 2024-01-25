473335c0c77a93bce37a710213e92ef7b6526b02
Jesse Dhillon <jesse@dhillon.com>
2024-01-24 23:18:36 -0800

    [nvim] move gnome-terminal watcher into tag-gtwatch

---
+++ tag-gtwatch/config/nvim/after/plugin/GnomeTerminalWatch.lua
@@ -0,0 +1,5 @@
+-- vim.g.gnome_terminal_colorschemes = {
+-- 	["e12a0f86-66d1-4616-8e50-4207baff7121"]="tokyonight-storm",
+-- 	["7c5fae27-55fe-4a48-b061-7e0a0bb1e82e"]="tokyonight-day",
+-- }
+-- vim.call('GnomeTerminalWatchProfile')
