69227f4823e3aaed0356cedd98ab29af08e43f79
Jesse Dhillon <jesse@dhillon.com>
2024-01-30 15:48:02 -0800

    [bashrc] refactor PS1

--- bash.d/99-ps1.sh
+++ bash.d/99-ps1.sh
@@ -1,206 +1,268 @@
 #!/bin/bash
 
 _update_ps1() {
-local black="\[\033[0;30m\]"
-local grey="\[\033[1;30m\]"
-local blue="\[\033[0;34m\]"
-local cyan="\[\033[0;36m\]"
-local green="\[\033[0;40m\]"
-local red="\[\033[0;31m\]"
-local purple="\[\033[0;35m\]"
-local brown="\[\033[0;33m\]"
-local yellow="\[\033[1;33m\]"
-local white="\[\033[1;37m\]"
+    local status=$?
 
-local positive_green="\[\033[38;5;64m\]"
-local lime="\[\033[38;5;148m\]"
-local pale_green="\[\033[38;5;191m\]"
-local electric_blue="\[\033[38;5;39m\]"
-local sky_blue="\[\033[38;5;81m\]"
-local scarlet="\[\033[38;5;9m\]"
-local dark_grey="\[\033[38;5;238m\]"
-local lavender="\[\033[38;5;59m\]"
-local light_cyan="\[\033[1;36m\]"
-local light_blue="\[\033[1;34m\]"
-local light_green="\[\033[1;32m\]"
-local light_red="\[\033[1;31m\]"
-local light_purple="\[\033[38;5;183m\]"
-local light_grey="\[\033[0;37m\]"
-local orange="\[\033[38;5;208m\]"
+    # basic terminal colors
+    local black="\[\033[38;5;0m\]"
+    local red="\[\033[38;5;1m\]"
+    local orange="\[\033[38;5;3m\]"
+    local green="\[\033[38;5;2m\]"
+    local blue="\[\033[38;5;4m\]"
+    local purple="\[\033[38;5;5m\]"
+    local cyan="\[\033[38;5;6m\]"
+    local white="\[\033[38;5;7m\]"
 
-local warning="\[\033[1;30m\]\[\033[48;5;208m\]"
-local danger="\[\033[38;5;195m\]\[\033[48;5;196m\]"
-local off="\[\033[0m\]"
+    # extended colors
+    local grey_offwhite="\[\033[38;5;241m\]"
+    local deep_red="\[\033[38;5;196m\]"
+    local sky_blue="\[\033[38;5;45m\]"
+    local electric_blue="\[\033[38;5;39m\]"
+    local indigo="\[\033[38;5;63m\]"
+    local midnight="\[\033[38;5;69m\]"
+    local lavender="\[\033[38;5;105m\]"
+    local azure="\[\033[38;5;123m\]"
+    local android="\[\033[38;5;79m\]"
+    local lilac="\[\033[38;5;153m\]"
+    local lime="\[\033[38;5;154m\]"
+    local burnt="\[\033[38;5;172m\]"
+    local acid="\[\033[38;5;194m\]"
+    local banana="\[\033[38;5;184m\]"
+    local tan="\[\033[38;5;231m\]"
+    local magenta="\[\033[38;5;207m\]"
+    local rose="\[\033[38;5;225m\]"
+    local pink="\[\033[38;5;219m\]"
+    local tangerine="\[\033[38;5;214m\]"
 
-angle_if() {
-    if [ -z "$1" ]; then
-        echo ""
+    # styles
+    local subtle=$grey_offwhite
+    local danger=$deep_red
+    local warning=$orange
+
+    local git_dirty_style=$red
+    local git_warn_style=$orange
+    local git_new_style=$cyan
+    local git_clean_style=$green
+    local git_stash_style=$sky_blue
+
+    local cwd_style=$rose
+    local jobs_style=$tangerine
+    local dirs_style=$electric_blue
+    local user_style=$acid
+
+    # reset
+    local off="\[\033[0m\]"
+
+    # surround, join, and space helper functions
+    angle_if() {
+        if [ -z "$1" ]; then
+            echo ""
+            return
+        fi
+
+        echo "<$1>"
         return
-    fi
+    }
 
-    echo "<$1>"
-    return
-}
+    bracket_if() {
+        if [ -z "$1" ]; then
+            echo ""
+            return
+        fi
 
-bracket_if() {
-    if [ -z "$1" ]; then
-        echo ""
+        echo "{$1}"
         return
-    fi
+    }
 
-    echo "{$1}"
-    return
-}
+    parens_if() {
+        if [ -z "$1" ]; then
+            echo ""
+            return
+        fi
 
-parens_if() {
-    if [ -z "$1" ]; then
-        echo ""
+        echo "($1)"
         return
-    fi
+    }
 
-    echo "($1)"
-    return
-}
-
-git_prompt() {
-    gitps1=$(__git_ps1)
-    echo $gitps1 | tr -d '()'
-}
-
-stash_length() {
-    echo $(git stash list 2> /dev/null | wc -l)
-}
-
-git_status() {
-    if [[ $(git st -s 2> /dev/null | grep -e "^\s*[DM]" | wc -l | awk '{ print $1 }') != "0" ]]; then
-        echo "modified"
-    elif [[ $(git st --porcelain 2> /dev/null | wc -l | awk '{ print $1 }') != "0" ]]; then
-        echo "untracked"
-    else
-        echo "clean"
-    fi
-}
-
-join() {
-    if [ -n "$2" -a -n "$3" ]; then
-        echo "$2$1$3"
-    elif [ -z "$2" -a -n "$3" ]; then
-        echo "$3"
-    elif [ -n "$2" -a -z "$3" ]; then
-        echo "$2"
-    fi
-}
-
-virtualenv_name() {
-    if [ -n "$VIRTUAL_ENV" ]; then
-        echo $(basename $VIRTUAL_ENV)
-    fi
-}
-
-prepend_if() {
-    if [ -n "$2" ]; then
-        if [ -n "$1" ]; then
-            echo "$1$2"
-        else
-            echo " $1"
+    join() {
+        if [ -n "$2" -a -n "$3" ]; then
+            echo "$2$1$3"
+        elif [ -z "$2" -a -n "$3" ]; then
+            echo "$3"
+        elif [ -n "$2" -a -z "$3" ]; then
+            echo "$2"
         fi
-    fi
-}
+    }
 
-append_if() {
-    if [ -n "$2" ]; then
-        if [ -n "$1" ]; then
-            echo "$2$1"
-        else
-            echo "$1 "
+    prepend_if() {
+        if [ -n "$2" ]; then
+            if [ -n "$1" ]; then
+                echo "$1$2"
+            else
+                echo " $1"
+            fi
         fi
-    fi
-}
+    }
 
-colorize() {
-    if [ -n "$2" ]; then
-        echo "$1$2$off"
-    fi
-}
-
-gem_set_name () {
-    [[ "$GEM_HOME" =~ .+@.+ ]] && echo $GEM_HOME | cut -d'@' -f2
-}
-
-blank_if_zero() {
-    if [ $1 -eq 0 ]; then
-        echo ""
-    else
-        echo $1
-    fi
-}
-
-    declare -A host_colors
-    host_colors=([jdhillon-x1]=$light_purple [claudius]=$light_purple [jdhillon-mbp.local]=$purple)
-    if [[ ${host_colors[$HOSTNAME]} ]]; then
-        host_color=${host_colors[$HOSTNAME]}
-    else
-        if [[ $TERM =~ "256color"  ]]; then
-            host_color="\[\033[38;5;$((16 + $(hostname | cksum | cut -c1-3) % 216))m\]";
-        else
-            host_color="\[\033[1;$((31 + $(hostname | cksum | cut -c1-3) % 6))m\]";
+    append_if() {
+        if [ -n "$2" ]; then
+            if [ -n "$1" ]; then
+                echo "$2$1"
+            else
+                echo "$1 "
+            fi
         fi
-    fi
+    }
 
-    declare -A awshash
-    awshash=([production]=prod [development]=dev [uat]=uat [integration2]=i2 [integration3]=i3)
-    awsenv=""
-    if [[ $AWS_ENVIRONMENT && ${awshash[$AWS_ENVIRONMENT]} ]]; then
-        awsenv=${awshash[$AWS_ENVIRONMENT]}
-        if [[ $awsenv == "prod" ]]; then
-            awscolor=$danger
+    blank_if_zero() {
+        if [ $1 -eq 0 ]; then
+            echo ""
         else
-            awscolor=$lavender
+            echo $1
         fi
-    fi
+    }
 
-    gs=$(git_status)
-    if [[ $gs == "modified" ]]; then
-        gitcolor=$scarlet
-    elif [[ $gs == "untracked" ]]; then
-        gitcolor=$electric_blue
-    else
-        gitcolor=$positive_green
-    fi
+    colorize() {
+        if [ -n "$2" ]; then
+            echo "$1$2$off"
+        fi
+    }
 
-    [[ $(stash_length) == "0" ]] && stashprompt="" || stashprompt="$(colorize $sky_blue "@$(stash_length)")${gitcolor}"
+    # various elements
+    virtualenv_name() {
+        if [ -n "$VIRTUAL_ENV" ]; then
+            echo $(basename $VIRTUAL_ENV)
+        fi
+    }
 
-    local venv=$(virtualenv_name)
-    local gemset=$(gem_set_name)
-    local branchname=$(git_prompt)
+    git_prompt() {
+        gitps1=$(__git_ps1)
+        echo $gitps1 | tr -d '()'
+    }
 
-    if [[ $branchname == *"|"* ]]; then
-        local gitstate=$(colorize $orange ^$(echo $branchname | cut -d'|' -f2))${gitcolor}
-        local branchname=$(echo $branchname | cut -d'|' -f1)
-        local gitprompt=${branchname}${gitstate}
-    else
-        local gitprompt=$branchname
-    fi
+    stash_length() {
+        echo $(git stash list 2> /dev/null | wc -l)
+    }
 
-    local env_name=`join "." $venv $gemset`
-    local joined=`join "$dark_grey|$gitcolor" $env_name ${gitprompt}${stashprompt}`
-    local bracketed=$(colorize $gitcolor "`append_if ' ' $(bracket_if $joined)`")
+    git_status() {
+        if [[ $(git st -s 2> /dev/null | grep -e "^\s*[DM]" | wc -l | awk '{ print $1 }') != "0" ]]; then
+            echo "modified"
+        elif [[ $(git st --porcelain 2> /dev/null | wc -l | awk '{ print $1 }') != "0" ]]; then
+            echo "untracked"
+        else
+            echo "clean"
+        fi
+    }
 
-    if [[ $USER == "root" ]]; then
-        local userhost=$(colorize $light_red "\u${off}${host_color}@\h")
-    else
-        local userhost=$(colorize $brown "\u${host_color}@\h")
-    fi
-    local cwd=$(colorize $cyan "\w")
-    local prompt=$(colorize $cyan '>')
+    git_branch_color() {
+        local gs=$(git_status)
+        if [[ $gs == "modified" ]]; then
+            gitcolor=$git_dirty_style
+        elif [[ $gs == "untracked" ]]; then
+            gitcolor=$git_new_style
+        else
+            gitcolor=$git_clean_style
+        fi
+        echo $gitcolor
+    }
 
-    local jobscount=$(colorize $lime $(prepend_if "%" `blank_if_zero $(jobs | wc -l)`))
-    local dirscount=$(colorize $blue $(append_if "#" `blank_if_zero $(dirs -v | cut -d' ' -f2 | sort -nr | head -1)`))
+    # PS1 components
+    git_branch_cmp() {
+        local gitcolor=$1
+        # get stash
+        if [[ $(stash_length) == "0" ]]; then
+            stashprompt=""
+        else
+            stashprompt="$(colorize $git_stash_style "@$(stash_length)")${gitcolor}"
+        fi
 
-    local timestamp=$(colorize $dark_grey "[`date +"%H:%M"`]")
-    local awsenv=$(colorize $awscolor "`append_if "$off " $(prepend_if "#" ${awsenv})`")
+        # get branch
+        local branchname=$(git_prompt)
+        if [[ $branchname == *"|"* ]]; then
+            # if the branch name has something like |^REBASE in it
+            local gitstate=$(colorize $git_warn_style ^$(echo $branchname | cut -d'|' -f2))${gitcolor}
+            local branchname=$(echo $branchname | cut -d'|' -f1)
+            local gitprompt=${branchname}${gitstate}
+        else
+            local gitprompt=$branchname
+        fi
+        echo "${gitprompt}${stashprompt}"
+    }
 
-    export PS1="${timestamp} ${awsenv}${bracketed}${userhost}${jobscount}${dark_grey}:${dirscount}${cwd}${prompt}${off} "
+    venv_branch_cmp() {
+        local gitcolor=$(git_branch_color)
+        local venv=$(virtualenv_name)
+        local gp=$(git_branch_cmp $gitcolor)
+
+        # in case we want to show multiple environments e.g. node version
+        local env_name=`join "." $venv`
+        local joined=`join "$subtle|$gitcolor" $env_name ${gp}`
+        echo $(colorize $gitcolor "`append_if ' ' $(bracket_if $joined)`")
+    }
+
+    user_host_cmp() {
+        declare -A host_colors
+        host_colors=([claudia]=$android [retro]=$burnt [jdhillon-mbp.local]=$purple)
+        if [[ ${host_colors[$HOSTNAME]} ]]; then
+            host_color=${host_colors[$HOSTNAME]}
+        else
+            if [[ $TERM =~ "256color"  ]]; then
+                host_color="\[\033[38;5;$((16 + $(hostname | cksum | cut -c1-3) % 216))m\]";
+            else
+                host_color="\[\033[1;$((31 + $(hostname | cksum | cut -c1-3) % 6))m\]";
+            fi
+        fi
+
+        if [[ $USER == "root" ]]; then
+            local userhost=$(colorize $danger "\u${off}${host_color}@\h")
+        else
+            local userhost=$(colorize $user_style "\u${off}${host_color}@\h")
+        fi
+        echo $userhost
+    }
+
+    cwd_cmp() {
+        echo $(colorize $cwd_style "\w")
+    }
+
+    prompt_cmp() {
+        local status=$1
+
+        local pc='$'
+        if [[ $UID == "0" ]]; then
+            pc='#'
+        fi
+
+        if [[ $status == "0" ]]; then
+            echo $(colorize $subtle $pc)
+        else
+            echo $(colorize $danger $pc)
+        fi
+    }
+
+    jobs_cmp() {
+        echo $(colorize $jobs_style $(prepend_if "%" `blank_if_zero $(jobs | wc -l)`))
+    }
+
+    dirs_cmp() {
+        echo $(colorize $dirs_style $(append_if "#" `blank_if_zero $(dirs -v | cut -d' ' -f2 | sort -nr | head -1)`))
+    }
+
+    timestamp_cmp() {
+        echo $(colorize $subtle "[`date +"%H:%M"`]")
+    }
+
+    # render pipeline
+    local cmps=( "$(timestamp_cmp) "
+                 "$(venv_branch_cmp)"
+                 $(user_host_cmp)
+                 $(jobs_cmp)
+                 "${subtle}:"
+                 $(dirs_cmp)
+                 $(cwd_cmp)
+                 $(prompt_cmp $status) )
+    local IFS=
+    export PS1="${cmps[*]}${off} "
 }
 
 _update_title () {
@@ -215,6 +277,7 @@ _update_title () {
 }
 
 foreground() {
+    # updates title when resuming jobs
     extglob_state=$(shopt -p extglob)
     shopt -s extglob
     case "$1" in
