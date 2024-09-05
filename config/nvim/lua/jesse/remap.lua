a9ea14969355292f81170efd4cf6ac2e253bfa6a
Jesse Dhillon <jesse@dhillon.com>
2024-09-05 09:54:57 -0700

    [nvim] add [c to jump to context

--- config/nvim/lua/jesse/remap.lua
+++ config/nvim/lua/jesse/remap.lua
@@ -81,5 +81,5 @@ vim.keymap.set("n", "<silent> S", ':exec "normal a".nr2char(getchar()).""<CR>')
 
 -- treesitter-context
 vim.keymap.set("n", "[c", function()
-  require("treesitter-context").go_to_context()
+  require("treesitter-context").go_to_context(vim.v.count1)
 end, { silent = true })
