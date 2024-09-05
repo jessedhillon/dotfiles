0be3ff8ca680a7307b8d41ccb582937aa28d7f73
Jesse Dhillon <jesse@dhillon.com>
2024-09-05 09:50:28 -0700

    [nvim] add vimgrep args to telescope, fzf extension (disabled)

--- config/nvim/after/plugin/telescope.lua
+++ config/nvim/after/plugin/telescope.lua
@@ -1,9 +1,29 @@
 local telescope = require('telescope')
 local builtin = require('telescope.builtin')
+local conf = require("telescope.config")
+
+-- Clone the default Telescope configuration
+local vimgrep_arguments = { table.unpack(conf.values.vimgrep_arguments) }
+
+-- I want to search in hidden/dot files.
+table.insert(vimgrep_arguments, "--hidden")
+
+-- I want to follow symbolic links
+table.insert(vimgrep_arguments, "-L")
 
 telescope.setup({
   defaults = {
     file_ignore_patterns = { "node_modules", "__pycache__", ".git" },
+		vimgrep_arguments = vimgrep_arguments,
+  },
+  extensions = {
+    fzf = {
+      fuzzy = true,                    -- false will only do exact matching
+      override_generic_sorter = true,  -- override the generic sorter
+      override_file_sorter = true,     -- override the file sorter
+      case_mode = "smart_case",        -- or "ignore_case" or "respect_case"
+                                       -- the default case_mode is "smart_case"
+    }
   },
   pickers = {
     find_files = {
@@ -12,6 +32,8 @@ telescope.setup({
     }
   },
 })
+
+-- require('telescope').load_extension('fzf')
 
 vim.keymap.set('n', '<C-p>', builtin.find_files, {})
 vim.keymap.set('n', '<C-f>', builtin.live_grep, {})
--- config/nvim/lua/plugins.lua
+++ config/nvim/lua/plugins.lua
@@ -5,9 +5,19 @@ return require('packer').startup(function(use)
   use('wbthomason/packer.nvim')
 
   use {
-    'nvim-telescope/telescope.nvim', tag = '0.1.4',
+    'nvim-telescope/telescope.nvim', branch = '0.1.x',
     requires = { {'nvim-lua/plenary.nvim'}, {'BurntSushi/ripgrep'} }
   }
+
+  use {
+    'nvim-telescope/telescope-fzf-native.nvim',
+    run = 'make'
+  }
+
+  -- use { "ibhagwan/fzf-lua",
+  --   -- optional for icon support
+  --   requires = { "nvim-tree/nvim-web-devicons" }
+  -- }
 
   -- use 'overcache/NeoSolarized'
   use 'folke/tokyonight.nvim'
