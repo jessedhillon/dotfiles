a93ea5269d90e2854948d840ae00993973bc730d
Jesse Dhillon <jesse@dhillon.com>
2024-09-05 09:45:13 -0700

    [tmux] update title

--- bash.d/99-tmux.sh
+++ bash.d/99-tmux.sh
@@ -10,3 +10,29 @@ if [ -n "$TMUX" ]; then
   }
   trap refresh DEBUG
 fi
+
+case ${TERM} in
+    tmux-*)
+        # user command to change default pane title on demand
+        function pane_title { TMUX_PANE_TITLE="$*"; }
+
+        # function that performs the title update (invoked as PROMPT_COMMAND)
+        # function update_title { printf "\033]2;%s\033\\" "${1:-$TMUX_PANE_TITLE}"; }
+        function _update_title {
+            if [ "${1:-$TMUX_PANE_TITLE}" = "_update_ps1" ]; then
+                printf "\033]2;%s\033\\" "bash";
+            else
+                printf "\033]2;%s\033\\" "${1:-$TMUX_PANE_TITLE}";
+            fi
+        }
+
+        # default pane title is the name of the current process (i.e. 'bash')
+        TMUX_PANE_TITLE=$(ps -o comm $$ | tail -1)
+
+        # Reset title to the default before displaying the command prompt
+        PROMPT_COMMAND=${PROMPT_COMMAND:+$PROMPT_COMMAND; }'_update_title'
+
+        # Update title before executing a command: set it to the command
+        trap '_update_title "$BASH_COMMAND"' DEBUG
+        ;;
+esac
